4b9b01d1a7aa15492ba2cb8929a86be4cf440325
Excigma <git@excigma.xyz>
2025-06-30 17:17:07 +1200

    feat: add estop and toggle inputs

--- src/tasks/led_task.cpp
+++ src/tasks/led_task.cpp
@@ -1,14 +1,45 @@
 #include "tasks/led_task.h"
 
+#define EMERGENCY_STOP_PIN 23
+#define TOGGLE_PIN 22
+
+volatile bool emergencyStopTriggered = false;
+volatile bool toggleTriggered = false;
+
+void IRAM_ATTR emergencyStopISR() {
+    emergencyStopTriggered = true;
+}
+
+void IRAM_ATTR toggleISR() {
+    toggleTriggered = true;
+}
+
 void ledTask(void *pvParameters)
 {
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(EMERGENCY_STOP_PIN, INPUT_PULLUP);
+    pinMode(TOGGLE_PIN, INPUT_PULLUP);
 
-    while (1)
-    {
-        digitalWrite(LED_BUILTIN, HIGH);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        digitalWrite(LED_BUILTIN, LOW);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    attachInterrupt(digitalPinToInterrupt(EMERGENCY_STOP_PIN), emergencyStopISR, FALLING);
+    attachInterrupt(digitalPinToInterrupt(TOGGLE_PIN), toggleISR, CHANGE);
+
+    bool ledState = false;
+    for (;;) {
+        if (emergencyStopTriggered) {
+            emergencyStopTriggered = false;
+            // Blink LED quickly 5 times
+            for (int i = 0; i < 5; ++i) {
+                digitalWrite(LED_BUILTIN, HIGH);
+                vTaskDelay(100 / portTICK_PERIOD_MS);
+                digitalWrite(LED_BUILTIN, LOW);
+                vTaskDelay(100 / portTICK_PERIOD_MS);
+            }
+        }
+        if (toggleTriggered) {
+            toggleTriggered = false;
+            ledState = !ledState;
+            digitalWrite(LED_BUILTIN, ledState ? HIGH : LOW);
+        }
+        vTaskDelay(10 / portTICK_PERIOD_MS); // Small delay to yield
     }
 }
