45600695d389387be4369bf02068f53a72453de2
Excigma <git@excigma.xyz>
2025-06-30 15:32:48 +1200

    feat: multicore uart & led test

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,42 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-
 #include <Arduino.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 2
 #endif
 
+TaskHandle_t ledTaskHandle = NULL;
+TaskHandle_t uartTaskHandle = NULL;
+
+void ledTask(void *pvParameters)
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    while (1)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        digitalWrite(LED_BUILTIN, LOW);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void uartTask(void *pvParameters)
+{
+    Serial.begin(115200);
+    while (1)
+    {
+        Serial.println("Hello!");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void setup()
 {
-    // initialize LED digital pin as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(115200);
+    xTaskCreatePinnedToCore(ledTask, "LED Task", 2048, NULL, 1, &ledTaskHandle, 0);
+    xTaskCreatePinnedToCore(uartTask, "UART Task", 2048, NULL, 1, &uartTaskHandle, 1);
 }
 
 void loop()
 {
-    // turn the LED on (HIGH is the voltage level)
-    digitalWrite(LED_BUILTIN, HIGH);
-    // wait for a second
-    delay(1000);
-    // turn the LED off by making the voltage LOW
-    digitalWrite(LED_BUILTIN, LOW);
-    // wait for a second
-    delay(1000);
-    Serial.println("Hello!");
+    // vTaskDelete(NULL); // Not used, delete default loop task
+    sleep(1);
 }
