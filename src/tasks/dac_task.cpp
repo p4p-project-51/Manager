8247c3fe9ac6081df96c9201bb8074f1cf65dd7f
Excigma <git@excigma.xyz>
2025-06-30 18:03:57 +1200

    feat: DAC output follows ADC input

--- src/tasks/dac_task.cpp
+++ src/tasks/dac_task.cpp
@@ -1,28 +1,24 @@
 #include "tasks/dac_task.h"
-#include <math.h>
 
 #define DAC1_PIN 25 // GPIO25 (DAC1)
 #define DAC2_PIN 26 // GPIO26 (DAC2)
-#define SINE_LOOKUP_TABLE_SIZE 128
+#define ADC_INPUT_PIN 34 // GPIO34 (ADC1_CH6, input only)
 
 void dacTask(void *pvParameters)
 {
-    uint8_t sineLookupTable[SINE_LOOKUP_TABLE_SIZE];
-    for (int i = 0; i < SINE_LOOKUP_TABLE_SIZE; ++i)
-    {
-        sineLookupTable[i] = (uint8_t)(127.5 * (sin(2 * M_PI * i / SINE_LOOKUP_TABLE_SIZE) + 1));
-    }
+    // Configure ADC input pin
+    pinMode(ADC_INPUT_PIN, INPUT);
 
-    int idx = 0;
     while (1)
     {
-        uint8_t val = sineLookupTable[idx];
+        int adcValue = analogRead(ADC_INPUT_PIN);
+        // Scale 12-bit to 8-bit (0-255)
+        uint8_t dacValue = adcValue >> 4;
 
         // Reduced resolution to workaround hardware bug on this DAC
-        dacWrite(DAC1_PIN, val / 2);
-        dacWrite(DAC2_PIN, val);
+        dacWrite(DAC1_PIN, dacValue / 2 + 127);
+        dacWrite(DAC2_PIN, dacValue);
 
-        idx = (idx + 1) % SINE_LOOKUP_TABLE_SIZE;
         vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
