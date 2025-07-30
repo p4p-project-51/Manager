c956f4b6725bef2141b40570b0d5a645eb533878
Excigma <git@excigma.xyz>
2025-07-30 18:21:44 +1200

    feat: read from four ads1015s

--- src/tasks/ads1x15_task.cpp
+++ src/tasks/ads1x15_task.cpp
@@ -2,24 +2,37 @@
 #include <Adafruit_ADS1X15.h>
 #include "tasks/ads1x15_task.h"
 
-Adafruit_ADS1015 ads1015;
+Adafruit_ADS1015 ads1015_0;
+Adafruit_ADS1015 ads1015_1;
+Adafruit_ADS1015 ads1015_2;
+Adafruit_ADS1015 ads1015_3;
 
+// Addresses:
+// ADDR -> GND 0x48 (default for unconnected ADDR pin)
+// ADDR -> VDD 0x49
+// ADDR -> SDA 0x4A
+// ADDR -> SCL 0x4B
 void pollAds1x15Task(void *pvParameters)
 {
     Serial.begin(115200);
-    ads1015.begin();
+    ads1015_0.begin(0x48);
+    ads1015_1.begin(0x49);
+    ads1015_2.begin(0x4A);
+    ads1015_3.begin(0x4B);
 
     while (1) {
-        int16_t adc[4];
-        for (int ch = 0; ch < 4; ++ch) {
-            adc[ch] = ads1015.readADC_SingleEnded(ch);
-        }
+        // Changing the data rate from the default 1600 SPS (either way) seems to negatively affect the readings
 
-        Serial.print("AIN0: "); Serial.print(adc[0]); Serial.print("\t");
-        Serial.print("AIN1: "); Serial.print(adc[1]); Serial.print("\t");
-        Serial.print("AIN2: "); Serial.print(adc[2]); Serial.print("\t");
-        Serial.print("AIN3: "); Serial.println(adc[3]);
+        int16_t adc0 = ads1015_0.readADC_SingleEnded(0);
+        int16_t adc1 = ads1015_1.readADC_SingleEnded(0);
+        int16_t adc2 = ads1015_2.readADC_SingleEnded(0);
+        int16_t adc3 = ads1015_3.readADC_SingleEnded(0);
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        Serial.print("ADC0_CH0: "); Serial.print(adc0); Serial.print("\t");
+        Serial.print("ADC1_CH0: "); Serial.print(adc1); Serial.print("\t");
+        Serial.print("ADC2_CH0: "); Serial.print(adc2); Serial.print("\t");
+        Serial.print("ADC3_CH0: "); Serial.println(adc3);
+
+        vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
