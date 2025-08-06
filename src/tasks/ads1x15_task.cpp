df8aa8131c367490ca1ebf6e0636d72a41c0cdcf
Excigma <git@excigma.xyz>
2025-08-06 14:17:28 +1200

    feat: construct json on esp32 directly

--- src/tasks/ads1x15_task.cpp
+++ src/tasks/ads1x15_task.cpp
@@ -3,6 +3,8 @@
 #include "tasks/ads1x15_task.h"
 #include "tasks/adc_send.h"
 #include "tasks/uart_task.h"
+#include <vector>
+#include <ArduinoJson.h>
 
 Adafruit_ADS1015 ads1015_modules[4];
 const uint8_t ads_addresses[4] = {0x48, 0x49, 0x4A, 0x4B};
@@ -14,9 +16,43 @@ const uint8_t num_channels = 4;
 // ADDR -> VDD 0x49
 // ADDR -> SDA 0x4A
 // ADDR -> SCL 0x4B
+String batchFirmwareId;
+void generateBatchFirmwareId()
+{
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    char buf[10];
+    for (int i = 0; i < 9; ++i)
+    {
+        buf[i] = charset[random(0, sizeof(charset) - 2)];
+    }
+    buf[9] = '\0';
+    batchFirmwareId = String(buf);
+}
+
+void sendBatchSamples(const std::vector<AdcSample> &batch)
+{
+    JsonDocument doc;
+    doc["id"] = batchFirmwareId;
+    doc["timestamp"] = (int)millis();
+    JsonArray header = doc["header"].to<JsonArray>();
+    JsonArray values = doc["values"].to<JsonArray>();
+    for (const auto &sample : batch)
+    {
+        char sensorName[16];
+        snprintf(sensorName, sizeof(sensorName), "M%d-C%d", sample.module, sample.channel);
+        header.add(sensorName);
+        values.add((int)sample.value);
+    }
+    String out;
+    serializeJson(doc, out);
+    uartSend(out.c_str());
+}
+
 void pollAds1x15Task(void *pvParameters)
 {
     Serial.begin(115200);
+    randomSeed(analogRead(0) + millis());
+    generateBatchFirmwareId();
 
     for (uint8_t i = 0; i < num_modules; ++i)
     {
@@ -26,7 +62,7 @@ void pollAds1x15Task(void *pvParameters)
 
     while (true)
     {
-        String batchLine;
+        std::vector<AdcSample> batch;
         for (uint8_t ch = 0; ch < num_channels; ++ch)
         {
             // Start conversion for all modules
@@ -48,15 +84,10 @@ void pollAds1x15Task(void *pvParameters)
                 }
 
                 int16_t adc = ads1015_modules[mod].getLastConversionResults();
-                char buf[32];
-                snprintf(buf, sizeof(buf), "M%d:C%d:V%d", mod, ch, adc);
-                if (!batchLine.isEmpty())
-                    batchLine += " ";
-                batchLine += buf;
+                batch.push_back({mod, ch, adc});
             }
         }
-
-        uartSend(batchLine.c_str());
+        sendBatchSamples(batch);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
